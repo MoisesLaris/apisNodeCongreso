b4bcd337d3c3b6c6a3ca825f4b329df8b07734f6
Moises Laris Santos <3119402@live.com>
2020-06-08 15:02:01 -0500

    Conexion bd atlas

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = 3800;
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/congreso', { useMongoClient: true })
+mongoose.connect('mongodb+srv://ProyectoCheco:proyectoch3c0@proyectocheco-v8da3.azure.mongodb.net/congreso?retryWrites=true&w=majority', { useMongoClient: true })
     .then(() => {
         console.log('Conexión exitosa! Gracias al moisi precioso');
 
