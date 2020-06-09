a8c4b754883a68abc624ab4cecbccb281bc47160
Moises Laris Santos <3119402@live.com>
2020-06-09 09:36:21 -0500

    cambios en api newUser

--- controllers/user.js
+++ controllers/user.js
@@ -27,28 +27,24 @@ function pruebas(req, res) {
 function newUser(req, res) {
     var params = req.body; //Toma todos los campos que llegan por req en body, y los pone en params
     var user = new User();
-    if (params.nombre && params.apellidos && params.tipoUsuario && params.correo && params.password && params.semestre && params.grupo) {
+    if (params.nombre && params.apellidos && params.correo && params.password && params.semestre && params.grupo) {
         //Seguir con el video jeje
-
-
 
         user.nombre = params.nombre;
         user.apellidos = params.apellidos;
-        user.tipoUsuario = params.tipoUsuario;
+        user.tipoUsuario = 1;
         user.correo = params.correo.toLowerCase();
         user.semestre = params.semestre;
         user.grupo = params.grupo;
 
         //Controlar los usuarios repetidos por correo
-        User.findOne({correo: user.correo.toLowerCase()}).exec((err, users) => {
+        User.findOne({ correo: user.correo.toLowerCase() }).exec((err, users) => {
             if (err) return res.status(500).send({ message: 'Error en peticion usuario ' + err })
-            if(users)
-            {
+            if (users) {
                 return res.status(200).send({
                     message: "El correo ya esta siendo usado por otro usuario."
                 });
-            }
-            else{
+            } else {
                 bcrypt.hash(params.password, null, null, (err, hash) => {
                     user.password = hash;
                     User.find({}).sort({ $natural: -1 }).exec(function(err, doc) {
@@ -68,13 +64,13 @@ function newUser(req, res) {
                             }
                         });
                     });
-        
+
                     //res.status(200).send({message:'Simon ' + valor.toString});
                 });
             }
         });
 
-        
+
     } else {
         res.status(200).send({
             message: "Hubo un problema al recibir los datos."
@@ -83,7 +79,7 @@ function newUser(req, res) {
 }
 
 //Log In
-function loginUser(req, res){
+function loginUser(req, res) {
     var params = req.body;
 
     var email = params.correo;
@@ -94,101 +90,90 @@ function loginUser(req, res){
         return res.status(200).send({message: 'Recibi esto '+params.correo + ' ' + params.password});
     }*/
 
-    User.findOne({correo: email},(err, user) =>{
-        if(err) return res.status(500).send({message: 'Error en la peticion'}); 
+    User.findOne({ correo: email }, (err, user) => {
+        if (err) return res.status(500).send({ message: 'Error en la peticion' });
         //return res.status(200).send({message: 'Recibi esto '+params.correo + ' ' + params.password});
-        if(user){
+        if (user) {
 
-            bcrypt.compare(password, user.password,(err, check) =>{
-                if(check){
+            bcrypt.compare(password, user.password, (err, check) => {
+                if (check) {
                     //Devolver datos de usuario
-                    if(params.getToken)
-                    {
+                    if (params.getToken) {
                         //Devolver token
                         //generar token
                         return res.status(200).send({
                             token: jwt.createToken(user)
                         });
-                    }
-                    else
-                    {
+                    } else {
                         //devolver datos de usuario
                     }
                     user.password = undefined;
-                    return res.status(200).send({user});
-                }
-                else
-                {
-                    return res.status(404).send({message: 'El usuario no se ha podido identificar'});  
+                    return res.status(200).send({ user });
+                } else {
+                    return res.status(404).send({ message: 'El usuario no se ha podido identificar' });
                 }
             });
-        }
-        else
-        {
-            return res.status(404).send({message: 'El usuario no existe'});  
+        } else {
+            return res.status(404).send({ message: 'El usuario no existe' });
         }
     });
 }
 
 //Consultar usuarios
-function getUser(req, res)
-{
+function getUser(req, res) {
     var userId = req.params.id;
 
     User.findById(userId, (err, user) => {
-        if(err) return res.status(500).send({message:'Error en la peticion'});
+        if (err) return res.status(500).send({ message: 'Error en la peticion' });
 
-        if(!user) return res.status(404).send({message: 'El usuario no existe'});
+        if (!user) return res.status(404).send({ message: 'El usuario no existe' });
 
-        return res.status(200).send({user});
+        return res.status(200).send({ user });
     });
 }
 
 //Consultar usuarios por paginas
-function getUsers(req, res)
-{
+function getUsers(req, res) {
     var identity_user_id = req.user.sub;
 
     var page = 1;
 
-    if(req.params.page){
+    if (req.params.page) {
         page = req.params.page;
     }
 
     var itemsPerPage = 5;
 
-    User.find().sort('_id').paginate(page,itemsPerPage,(err,users,total) => {
-        if(err) return res.status(500).send({message:'Error en la peticion'});
+    User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
+        if (err) return res.status(500).send({ message: 'Error en la peticion' });
 
-        if(!users) return res.status(404).send({message:'No hay usuarios disponibles'});
+        if (!users) return res.status(404).send({ message: 'No hay usuarios disponibles' });
 
         return res.status(200).send({
             users,
             total,
-            pages: Math.ceil(total/itemsPerPage),
+            pages: Math.ceil(total / itemsPerPage),
         });
     });
 
 }
 
 //Actualizar usuario
-function updateUser(req,res)
-{
+function updateUser(req, res) {
     var userId = req.params.id;
     var update = req.body;
 
     //borrar propiedad password
     delete update.password;
 
-    if(userId != req.user.sub)
-    {
-        return res.status(200).send({message: 'No tienes permisos para esto'});
+    if (userId != req.user.sub) {
+        return res.status(200).send({ message: 'No tienes permisos para esto' });
     }
 
-    User.findByIdAndUpdate(userId,update,{new:true},(err, userUpdated) => {
-        if(err) return res.status(500).send({message:'Error en la peticion'});
+    User.findByIdAndUpdate(userId, update, { new: true }, (err, userUpdated) => {
+        if (err) return res.status(500).send({ message: 'Error en la peticion' });
 
-        if(!userUpdated) return res.status(404).send({message:'No se ha podido actualizar'});
+        if (!userUpdated) return res.status(404).send({ message: 'No se ha podido actualizar' });
 
         return res.status(200).send({
             user: userUpdated,
@@ -197,8 +182,7 @@ function updateUser(req,res)
 }
 
 //Borrar usuario
-function deleteUser(req,res)
-{
+function deleteUser(req, res) {
     var tipoUsuario = req.user.tipoUsuario;
     var usuario = req.user.sub;
 
@@ -208,9 +192,9 @@ function deleteUser(req,res)
     }*/
 
     User.findById(usuario).remove(err => {
-        if(err) return res.status(500).send({message: 'Error al eliminar usuario'});
+        if (err) return res.status(500).send({ message: 'Error al eliminar usuario' });
 
-        return res.status(200).send({message:'Usuario Eliminado'});
+        return res.status(200).send({ message: 'Usuario Eliminado' });
     });
 }
 module.exports = {
