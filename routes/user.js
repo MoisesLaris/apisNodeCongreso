3d41a604a0d9d6ff73c2803c325adbb6f894c71c
Jorge Armando Sandoval Contreras <j4cksn0w@MacBook-Air-de-Armando.local>
2020-06-11 14:39:25 -0500

    Cambio en la paginacion con ruta

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ api.get('/pruebas', md_auth.ensureAuth, userController.pruebas);
 api.post('/newUser', userController.newUser);
 api.post('/login', userController.loginUser);
 api.get('/getUser/:id', md_auth.ensureAuth, userController.getUser);
-api.get('/getUsers/:page?', md_auth.ensureAuth, userController.getUsers);
+api.get('/getUsers', md_auth.ensureAuth, userController.getUsers);
 api.put('/updateUser/:id', md_auth.ensureAuth, userController.updateUser);
 api.put('/deleteUser/:id', md_auth.ensureAuth, userController.deleteUser);
 api.get('/getUserByToken', md_auth.ensureAuth, userController.getUserByToken);
