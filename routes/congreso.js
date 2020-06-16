4a11e806117c3a8750f63e8056eb47998e2055c3
Moises Laris Santos <3119402@live.com>
2020-06-16 12:20:24 -0500

    change

--- routes/congreso.js
+++ routes/congreso.js
@@ -5,11 +5,11 @@ var congresoController = require('../controllers/congreso');
 
 var api = express.Router();
 
-api.post('/newCongreso',congresoController.newCongreso);
-api.get('/getCongreso/:id',congresoController.getCongreso);
-api.get('/getCongresos',congresoController.getCongresos);
-api.post('/updateCongreso/:id',congresoController.updateCongreso);
-api.post('/deleteCongreso/:id',congresoController.deleteCongreso);
-api.get('/getCongresos/idCongreso/:id',congresoController.getCongresosCarrera);
+api.post('/newCongreso', congresoController.newCongreso);
+api.get('/getCongreso/:id', congresoController.getCongreso);
+api.get('/getCongresos', congresoController.getCongresos);
+api.post('/updateCongreso/:id', congresoController.updateCongreso);
+api.post('/deleteCongreso/:id', congresoController.deleteCongreso);
+api.get('/getCongresos/idCarrera/:id', congresoController.getCongresosCarrera);
 
 module.exports = api;
