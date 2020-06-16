e26186dfcb3a30d6023b6d378c36f407f8ae483e
Moises Laris Santos <3119402@live.com>
2020-06-16 11:38:13 -0500

    post -> get api/getfaq/idcongreso

--- routes/FAQ.js
+++ routes/FAQ.js
@@ -7,11 +7,11 @@ var api = express.Router();
 
 var md_auth = require('../middleware/authenticated');
 
-api.post('/newFAQ',md_auth.ensureAuth, FAQController.newFAQ);
-api.get('/getFaq/:id',md_auth.ensureAuth, FAQController.getFaq);
-api.get('/getFaqs',md_auth.ensureAuth, FAQController.getFaqs);
-api.post('/updateFaq/:id',md_auth.ensureAuth, FAQController.updateFaq);
-api.post('/deleteFaq/:id',md_auth.ensureAuth, FAQController.deleteFaq);
-api.post('/getFaq/idCongreso/:id',md_auth.ensureAuth, FAQController.getFaqsCongreso);
+api.post('/newFAQ', md_auth.ensureAuth, FAQController.newFAQ);
+api.get('/getFaq/:id', md_auth.ensureAuth, FAQController.getFaq);
+api.get('/getFaqs', md_auth.ensureAuth, FAQController.getFaqs);
+api.post('/updateFaq/:id', md_auth.ensureAuth, FAQController.updateFaq);
+api.post('/deleteFaq/:id', md_auth.ensureAuth, FAQController.deleteFaq);
+api.get('/getFaq/idCongreso/:id', md_auth.ensureAuth, FAQController.getFaqsCongreso);
 
 module.exports = api;
