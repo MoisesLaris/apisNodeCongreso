30eea6375ce98057f87c036eb0b6e2f039ef82fa
Jorge Armando Sandoval Contreras <j4cksn0w@MacBook-Air-de-Armando.local>
2020-06-17 11:21:50 -0500

    cambio en ruta de actividades para traer actividades por id congreso

--- routes/actividad.js
+++ routes/actividad.js
@@ -8,7 +8,7 @@ var api = express.Router();
 
 api.post('/newActividad',actividadController.newActividad);
 api.get('/getActividad/:id',actividadController.getActividad);
-api.get('/getActividades/:id',actividadController.getActividadesCongreso);
+api.get('/getActividades/idCongreso/:id',actividadController.getActividadesCongreso);
 api.post('/updateActividad/:id',actividadController.updateActividad);
 api.post('/deleteActividad/:id',actividadController.deleteActividad);
 
