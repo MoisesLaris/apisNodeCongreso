552dd4d45d4673c42beb399dd3b77305cd0c24f6
Moises Laris Santos <3119402@live.com>
2020-06-20 14:20:47 -0500

    pago

--- routes/pago.js
+++ routes/pago.js
@@ -6,12 +6,12 @@ var pagoController = require('../controllers/pago');
 var api = express.Router();
 
 
-api.post('/newAPago',pagoController.newPago);
-api.get('/getPagos/:id',pagoController.getPago);
-api.get('/getPagos/idUsuario/:id',pagoController.getPagosUsuario);
-api.get('/getPagos/idTipoPago/:id',pagoController.getPagosTipoPago);
-api.get('/getPagos/idCongreso/:id',pagoController.getPagosCongreso);
-api.get('/updatePagos/:id',pagoController.updatePago);
+api.post('/newPago', pagoController.newPago);
+api.get('/getPagos/:id', pagoController.getPago);
+api.get('/getPagos/idUsuario/:id', pagoController.getPagosUsuario);
+api.get('/getPagos/idTipoPago/:id', pagoController.getPagosTipoPago);
+api.get('/getPagos/idCongreso/:id', pagoController.getPagosCongreso);
+api.get('/updatePagos/:id', pagoController.updatePago);
 
 
 module.exports = api;
