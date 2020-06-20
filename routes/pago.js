08e066e70c1665d8c034af2697a413ca36a89654
Jorge Armando Sandoval Contreras <j4cksn0w@MacBook-Air-de-Armando.local>
2020-06-20 15:49:19 -0500

    cambio a post

--- routes/pago.js
+++ routes/pago.js
@@ -11,7 +11,7 @@ api.get('/getPagos/:id', pagoController.getPago);
 api.get('/getPagos/idUsuario/:id', pagoController.getPagosUsuario);
 api.get('/getPagos/idTipoPago/:id', pagoController.getPagosTipoPago);
 api.get('/getPagos/idCongreso/:id', pagoController.getPagosCongreso);
-api.get('/updatePagos/:id', pagoController.updatePago);
+api.post('/updatePagos/:id', pagoController.updatePago);
 
 
 module.exports = api;
